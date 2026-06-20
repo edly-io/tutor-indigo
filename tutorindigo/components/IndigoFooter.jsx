31ec0b5e79a0dc3e8ffe7e74ceae8dde90cc0640
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2026-06-20 11:44:37 +0500

    fix: insta link, correct order & mismatches and remove unused messages

--- tutorindigo/components/IndigoFooter.jsx
+++ tutorindigo/components/IndigoFooter.jsx
@@ -4,21 +4,6 @@ const IndigoFooter = () => {
   const config = getConfig();
 
   const messages = {
-    "footer.poweredby.text": {
-      id: "footer.poweredby.text",
-      defaultMessage: "Powered by",
-      description: "text for the footer",
-    },
-    "footer.tutorlogo.altText": {
-      id: "footer.tutorlogo.altText",
-      defaultMessage: "Runs on Tutor",
-      description: "alt text for the footer tutor logo",
-    },
-    "footer.logo.altText": {
-      id: "footer.logo.altText",
-      defaultMessage: "Powered by Open edX",
-      description: "alt text for the footer logo.",
-    },
     "footer.copyright.text": {
       id: "footer.copyright.text",
       defaultMessage: "{organization} ©{year}. All Rights Reserved.",
@@ -29,54 +14,24 @@ const IndigoFooter = () => {
       defaultMessage: "About Us",
       description: "Footer navigation link",
     },
-    "footer.navlink.blog": {
-      id: "footer.navlink.blog",
-      defaultMessage: "Blog",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.donate": {
-      id: "footer.navlink.donate",
-      defaultMessage: "Donate",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.termsOfService": {
-      id: "footer.navlink.termsOfService",
-      defaultMessage: "Terms of Service",
-      description: "Footer navigation link",
-    },
     "footer.navlink.privacyPolicy": {
       id: "footer.navlink.privacyPolicy",
       defaultMessage: "Privacy Policy",
       description: "Footer navigation link",
     },
-    "footer.navlink.help": {
-      id: "footer.navlink.help",
-      defaultMessage: "Help",
+    "footer.navlink.joinAsPartner": {
+      id: "footer.navlink.joinAsPartner",
+      defaultMessage: "Join as Partner",
+      description: "Footer navigation link",
+    },
+    "footer.navlink.joinAsInstructor": {
+      id: "footer.navlink.joinAsInstructor",
+      defaultMessage: "Join as Instructor",
       description: "Footer navigation link",
     },
     "footer.navlink.contactUs": {
       id: "footer.navlink.contactUs",
       defaultMessage: "Contact Us",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.courseCatalog": {
-      id: "footer.navlink.courseCatalog",
-      defaultMessage: "Course Catalog",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.instructorJoin": {
-      id: "footer.navlink.instructorJoin",
-      defaultMessage: "Instructor Join",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.partners": {
-      id: "footer.navlink.partners",
-      defaultMessage: "Partners",
-      description: "Footer navigation link",
-    },
-    "footer.navlink.partnersJoin": {
-      id: "footer.navlink.partnersJoin",
-      defaultMessage: "Partners Join",
       description: "Footer navigation link",
     },
   };
@@ -93,7 +48,7 @@ const IndigoFooter = () => {
               </a>
               <ul>
                 <li>
-                  <a href="https://www.instagram.com/rwaq/" target="_blank">
+                  <a href="https://www.instagram.com/rwaq_/" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none"><mask id="path-1-inside-1_7278_2079" fill="white"><path d="M0 20C0 8.95431 8.95431 0 20 0C31.0457 0 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.95431 40 0 31.0457 0 20Z"></path></mask><path d="M0 20M40 20M40 20M0 20M20 0M40 20M20 40M0 20M20 40V39C9.50659 39 1 30.4934 1 20H0H-1C-1 31.598 8.40202 41 20 41V40ZM40 20H39C39 30.4934 30.4934 39 20 39V40V41C31.598 41 41 31.598 41 20H40ZM20 0V1C30.4934 1 39 9.50659 39 20H40H41C41 8.40202 31.598 -1 20 -1V0ZM20 0V-1C8.40202 -1 -1 8.40202 -1 20H0H1C1 9.50659 9.50659 1 20 1V0Z" fill="#4A5565" mask="url(#path-1-inside-1_7278_2079)"></path><path d="M24.1667 11.667H15.8334C13.5322 11.667 11.6667 13.5325 11.6667 15.8337V24.167C11.6667 26.4682 13.5322 28.3337 15.8334 28.3337H24.1667C26.4679 28.3337 28.3334 26.4682 28.3334 24.167V15.8337C28.3334 13.5325 26.4679 11.667 24.1667 11.667Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M23.3333 19.4753C23.4361 20.1688 23.3176 20.8771 22.9947 21.4994C22.6718 22.1218 22.1609 22.6264 21.5346 22.9416C20.9083 23.2569 20.1986 23.3666 19.5064 23.2552C18.8142 23.1438 18.1747 22.817 17.679 22.3212C17.1832 21.8255 16.8564 21.186 16.745 20.4938C16.6336 19.8015 16.7433 19.0918 17.0585 18.4656C17.3737 17.8393 17.8784 17.3284 18.5007 17.0055C19.1231 16.6825 19.8314 16.5641 20.5249 16.6669C21.2324 16.7718 21.8873 17.1015 22.393 17.6072C22.8987 18.1129 23.2283 18.7678 23.3333 19.4753Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M24.5833 15.417H24.5916" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                   </a>
                 </li>
@@ -117,28 +72,8 @@ const IndigoFooter = () => {
             <nav className="privacy-nav">
               <ul>
                 <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/courses/`}>
-                    {intl.formatMessage(messages["footer.navlink.courseCatalog"])}
-                  </a>
-                </li>
-                <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/contact-us/`}>
-                    {intl.formatMessage(messages["footer.navlink.contactUs"])}
-                  </a>
-                </li>
-                <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/instructor-join/`}>
-                    {intl.formatMessage(messages["footer.navlink.instructorJoin"])}
-                  </a>
-                </li>
-                <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/partners/`}>
-                    {intl.formatMessage(messages["footer.navlink.partners"])}
-                  </a>
-                </li>
-                <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/partners-join/`}>
-                    {intl.formatMessage(messages["footer.navlink.partnersJoin"])}
+                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/about/`}>
+                    {intl.formatMessage(messages["footer.navlink.aboutUs"])}
                   </a>
                 </li>
                 <li>
@@ -147,8 +82,18 @@ const IndigoFooter = () => {
                   </a>
                 </li>
                 <li>
-                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/about/`}>
-                    {intl.formatMessage(messages["footer.navlink.aboutUs"])}
+                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/partners-join/`}>
+                    {intl.formatMessage(messages["footer.navlink.joinAsPartner"])}
+                  </a>
+                </li>
+                <li>
+                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/instructor-join/`}>
+                    {intl.formatMessage(messages["footer.navlink.joinAsInstructor"])}
+                  </a>
+                </li>
+                <li>
+                  <a href={`${config.ENABLE_HTTPS ? 'https' : 'http'}://site.${config.BASE_URL}/contact-us/`}>
+                    {intl.formatMessage(messages["footer.navlink.contactUs"])}
                   </a>
                 </li>
               </ul>
